dfb0920e688d594d05c7e274784d54b2c94a78bb
Esme Tranquilino <etranquilino07@gmail.com>
2025-02-23 15:39:00 -0800

    updated JS

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+function updateWeather (res) {
+    let temperature = document.querySelector("#temperature")
+    let currentTemp = res.data.temperature.current;
+    let city = document.querySelector("#display-city");
+    city.innerHTML = res.data.city
+
+    temperature.innerHTML = Math.round(currentTemp)
+    console.log(currentTemp)
+}
+
+
+function searchCity (city) {
+    let apiKey = "016a0t85f68c4oda9e60b9cd7345774b"
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    // console.log(apiUrl);
+    axios.get(apiUrl).then(updateWeather);
+}
+
+
 function searchSubmit (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input")
-    let city = document.querySelector("#display-city")
-    city.innerHTML = searchInput.value;
+    searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form")
 searchFormElement.addEventListener("submit", searchSubmit)
+
+searchCity("Los Angeles")
